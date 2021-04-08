4540eda233c7f7aeea42a50327312223bd575386
faradawn <faradawny@gmail.com>
2021-04-08 23:50:53 +0800

    first template

--- letters/urls.py
+++ letters/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.letter_home, name='letter_home'),
+    path('', views.home, name='home'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result/', views.result, name='result'),
 ]
--- letters/views.py
+++ letters/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Questions
 
 # Create your views here.
 
-def letter_home(request):
-    return HttpResponse('Welcome to letter_home') 
+def home(request):
+    latest_q_list = Questions.objects.order_by('-pub_date')[:5]
+    context = {'latest': latest_q_list}
+    return render(request, 'letters/home.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'letters/detail.html', {'question': question})
+
+def result(request, question_id):
+    return HttpResponse('Result for Question %s is ' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Vote count for Question %s: ' % question_id)
+    
